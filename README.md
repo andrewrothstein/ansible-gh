ca93e96c35741e45a701cb5b8e779a93bb0694ee
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-07-15 23:39:29 -0400

    gh:1.12.1

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.gh
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-gh/actions/workflows/build.yml/badge.svg)
+
+Installs the GitHub cli [gh](https://cli.github.com/)
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.gh
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/cli/cli/releases/download
+
+# https://github.com/cli/cli/releases/download/v1.12.1/gh_1.12.1_checksums.txt
+# https://github.com/cli/cli/releases/download/v1.12.1/gh_1.12.1_linux_amd64.tar.gz
+
+dl() {
+    local ver=$1
+    local lchecksums=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
+    local platform="${os}_${arch}"
+    local file="gh_${ver}_${platform}.${archive_type}"
+    local url=$MIRROR/v$ver/$file
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    local checksums="gh_${ver}_checksums.txt"
+    local url=$MIRROR/v$ver/$checksums
+    local lchecksums=$DIR/$checksums
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s':\n" $ver
+
+    dl $ver $lchecksums linux 386
+    dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums linux arm64
+    dl $ver $lchecksums linux armv6
+    dl $ver $lchecksums macOS amd64
+    dl $ver $lchecksums windows amd64 zip
+}
+
+dl_ver ${1:-1.12.1}
